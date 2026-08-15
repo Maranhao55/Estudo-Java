5732e10c6c13d818c2f3117fa282e6b99b271bab
Maranhao 55 <149899127+Maranhao55@users.noreply.github.com>
2026-08-15 11:17:24 -0300

    Add files via upload

---
+++ AprovadoReprovado.java
@@ -0,0 +1,10 @@
+/*
+ * Leia 3 notas de um aluno e imprima se ele foi aprovado ou não.
+ * Obs: O aluno deve possuir média 7.0 ou maior para ser aprovado.
+ */
+public class AprovadoReprovado {
+  public static void main(String[] args) {
+    // Escreva seu zxvfsdvsdbvcódigo aqui...
+  }
+  
+}
