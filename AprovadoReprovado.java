ab97478dc6523577ce2be098e8251deea4b2024f
Maranhao 55 <149899127+Maranhao55@users.noreply.github.com>
2026-08-15 11:19:14 -0300

    Add functionality to check student approval status
    Implement logic to read student grades and determine approval status based on average.

--- AprovadoReprovado.java
+++ AprovadoReprovado.java
@@ -2,9 +2,32 @@
  * Leia 3 notas de um aluno e imprima se ele foi aprovado ou não.
  * Obs: O aluno deve possuir média 7.0 ou maior para ser aprovado.
  */
+
+import java.util.Scanner;
+
 public class AprovadoReprovado {
-  public static void main(String[] args) {
-    // Escreva seu zxvfsdvsdbvcódigo aqui...
-  }
-  
+
+public static void main (String[] args) {
+    
+    System.out.println("Digite suas 3 notas: ");
+    
+    Scanner scanner = new Scanner(System.in) ;
+    
+    double nota1 = scanner.nextDouble();
+    double nota2 = scanner.nextDouble();
+    double nota3 = scanner.nextDouble();
+
+    double media = (nota1 + nota2 + nota3) / 3;
+
+    if (media >= 7.0) {
+        System.out.println("Parabéns! Você foi aprovado com média: " + media);
+    } else {
+        System.out.println("Você foi reprovado com média: " + media);
+    }
+
+scanner.close();
+
+
+}
+    
 }
